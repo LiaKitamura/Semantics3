c79a7d7f8abc5db0e39909e6107c167fd410730c
Lia Kitamura <kit4mura@gmail.com>
2014-08-16 07:48:43 -0600

    deleted extra note to myself

--- lib/sem3_importer.rb
+++ lib/sem3_importer.rb
@@ -5,33 +5,16 @@ class Sem3Importer
   @api_key='API_KEY_HERE'
   @api_secret='API_SECRET_HERE'
 
-  # Trying to save just top level category names here. Now that I think about it I don't think this is necessary.
-
-  # def self.categories
-  #   sem3 = Semantics3::Products.new(@api_key,@api_secret)
-  #   sem3.categories_field("name", "Sporting Equipment")
-  #
-  #   constructedJson = sem3.get_query_json( "categories" )
-  #   categoriesHash = sem3.get_categories
-  #   category = categoriesHash
-  #   cat_results = category["results"]
-  #   final = cat_results.first
-  #
-  #   Category.find_or_create_by(main_category_name: final["name"], main_category_id: final["cat_id"])
-  #
-  # end
-
-
   # This is not the ideal way to get the data from the DB. But works for now while I work on cleaning this up
 
   def self.products
     sem3 = Semantics3::Products.new(@api_key,@api_secret)
-
+    
     sem3.products_field( "name", "tents" )
     sem3.products_field( "name", "knives and tools" )
     sem3.products_field( "name", "downhill skiing" )
     # sem3.products_field( "name", "goggles" )
-    
+
     constructedJson = sem3.get_query_json( "products" )
     productsHash = sem3.get_products
     all = productsHash["results"]
